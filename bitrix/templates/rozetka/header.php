469f4eef54531f2ef315bedfab8eceb4f398a358
super-gr0ver <e.dorodnov@gmail.com>
2023-11-06 01:00:30 +0300

    CSS fix

--- bitrix/templates/rozetka/header.php
+++ bitrix/templates/rozetka/header.php
@@ -8,7 +8,7 @@ IncludeTemplateLangFile(__FILE__);
     use Bitrix\Main\Page\Asset;
     Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/css/style.css');
     $APPLICATION->ShowHead();
-    $APPLICATION->ShowPanel()
+    $APPLICATION->ShowPanel();
   ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,8 +17,15 @@ IncludeTemplateLangFile(__FILE__);
 </head>
 <body>
   <div class="rootcontainer">
-  <svg class="icon">
-    <use xlink:href="#rozetkalogo"></use>
-  </svg>
+    <span class="tagline">Электрика свет</span>
+    <div class="logo_header">
+      <svg class="icon">
+        <use xlink:href="#rozetkalogo"></use>
+      </svg>
+    </div>
+    
+    <div class="catalog_header">2</div>
+    <div class="contacts_header">3</div>
+  
 
   </div>
